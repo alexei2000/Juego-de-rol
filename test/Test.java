c7131193e6490cc17aab841a97f2435fd71a3240
alexei hernandez <alexei2000.13@gmail.com>
2019-12-09 13:09:22 -0400

    gitignore

--- test/Test.java
+++ test/Test.java
@@ -3,8 +3,7 @@ import javax.swing.*;
 
 public class Test{
     public static void main(String[] args){
-        ImageIcon img = new ImageIcon("wizard.jpg");
-        JOptionPane.showMessageDialog(null,"hola","stats",JOptionPane.PLAIN_MESSAGE,img);
+    
     }
 }
 
